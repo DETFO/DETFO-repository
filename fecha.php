0f947ef8f5e50ff026c01c80a1abd68ecc176daa
DETFO <daniel20469@cbtis75.edu.mx>
2022-09-30 09:15:06 -0500

    30

---
+++ fecha.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>fecha</title>
+    <link rel="stylesheet" href="estilo1.css">
+</head>
+<center>
+<body>
+    <?php
+        echo '<h1>';
+        echo date('d-m-y');
+        echo '</h1>';
+    ?>
+</body>
+</center>
+</html> 
